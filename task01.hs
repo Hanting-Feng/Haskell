ba6050e91717306fffb20cb64e1e7d6b57d98537
Hanting-Feng <fenghanting1212@gmail.com>
2025-05-21 22:54:45 +0200

    Remove .dist folder

--- task01.hs
+++ task01.hs
@@ -1,16 +1,31 @@
-module Task01 where
+module Task04 where
 
 import CodeWorld
-import Prelude hiding (($)) 
+import Data.Text (pack)
+import Prelude hiding (($))
 
-scene :: Picture
-scene = rechteck & translated 0 5 (rund)
+scene :: Double -> Picture
+scene t
+  | t <= 1 = rechteck & translated (8 * t) (7 * t - 9.81 / 2 * t ^ 2) (rund)
+  | otherwise = rechteck & translated 10 0 (rund)
 
 rund :: Picture
-rund = colored yellow (solidCircle 1)
+rund = translated (-5) 1 (colored yellow (solidCircle 1))
 
 rechteck :: Picture
 rechteck = colored green (solidRectangle 15 2)
 
+-- Do not change the stuff below here!
+sceneWithTime :: Double -> Picture
+sceneWithTime t = countTime t & scene t
+
 main :: IO ()
-main = drawingOf scene
+main = animationOf sceneWithTime
+
+countTime :: Double -> Picture
+countTime t = dilated 0.5 (translated 15 (-6) (lettering (pack ("t = " ++ truncatedTime t))))
+
+truncatedTime :: Double -> String
+truncatedTime t =
+  let (n, f) = properFraction t
+   in show (n :: Int) ++ take 3 (tail (show f))
